94127d3a5814894344054b5583173c2eaf6c5fb7
Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>
2023-08-15 21:10:09 +0300

    lesson 18

---
+++ src/test/java/lesson18/annotations/testng/TestAnnotation.java
@@ -0,0 +1,57 @@
+package lesson18.annotations.testng;
+
+import org.testng.annotations.*;
+
+public class TestAnnotation {
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("Before Suite");
+    }
+
+    @BeforeTest
+    public void beforeTest(){
+        System.out.println("Before Test");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("Before Class");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("Before Method");
+    }
+
+    @Test
+    public void test(){
+        System.out.println("First test");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Second test");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("After Method");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("After Class");
+    }
+
+    @AfterTest
+    public void afterTest(){
+        System.out.println("After Test");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("After Suite");
+    }
+
+}
