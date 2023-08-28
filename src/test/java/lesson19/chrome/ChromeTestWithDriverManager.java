597d92b2d80b79e77f7e2aab088f934b95425ed7
Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>
2023-08-28 15:01:18 +0300

    fix

--- src/test/java/lesson19/chrome/ChromeTestWithDriverManager.java
+++ src/test/java/lesson19/chrome/ChromeTestWithDriverManager.java
@@ -1,7 +1,9 @@
 package lesson19.chrome;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -18,8 +20,15 @@ public class ChromeTestWithDriverManager {
     }
 
     @Test
-    public void testOnChrome(){
+    public void testOnChrome() throws InterruptedException {
         driver.get("https://www.google.com/");
+        WebElement inputSearch = driver.findElement(By.name("q"));
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+        Thread.sleep(3000);
+        inputSearch.sendKeys("Hillel");
+        Thread.sleep(3000);
+        searchButton.click();
+        Thread.sleep(2000);
     }
 
     @AfterTest
