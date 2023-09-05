b1405ef752a62f7b5041b81d7e46f9d6a5d89e2e
antifloodbot <36970838+antifloodbot@users.noreply.github.com>
2023-09-05 20:00:34 +0300

    Homework26 (#5)
    * homework 26
    
    * homework 26
    
    * homework 26
    
    * homework 26
    
    * homework 26
    
    * homework 26
    
    * homework 26
    
    ---------
    
    Co-authored-by: Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>

---
+++ src/main/java/ua/hillel/homework/hw26/homework26.java
@@ -0,0 +1,56 @@
+package ua.hillel.homework.hw26;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class homework26 {
+    WebDriver driver;
+    WebDriverWait webDriverWait;
+
+    @BeforeTest
+    public void setupWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5000));
+    }
+
+    @Test
+    public void testLocators() {
+        driver.get("https://rozetka.com.ua/");
+        WebElement logoCss = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("img[alt='Rozetka Logo']")));
+        logoCss.click();
+        WebElement logoXPath = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@alt='Rozetka Logo']")));
+        logoXPath.click();
+        WebElement authButtonCss = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("rz-user[class='header-actions__component']")));
+        authButtonCss.click();
+        WebElement closeAuthPopupCss = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[aria-label='Закрыть модальное окно']")));
+        closeAuthPopupCss.click();
+        WebElement authButtonXPath = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//rz-user/button")));
+        authButtonXPath.click();
+        WebElement closeAuthPopupXPath = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='modal__close']")));
+        closeAuthPopupXPath.click();
+        WebElement basketCss = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("rz-cart[class='header-actions__component']")));
+        basketCss.click();
+        WebElement closeBasketPopupCss = webDriverWait.until((ExpectedConditions.elementToBeClickable(By.cssSelector("button[aria-label='Закрыть модальное окно']"))));
+        closeBasketPopupCss.click();
+        WebElement basketXPath = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.xpath("//rz-cart/button")));
+        basketXPath.click();
+        WebElement closeBasketPopupXpath = webDriverWait.until((ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='modal__close']"))));
+        closeBasketPopupXpath.click();
+    }
+
+    @AfterTest
+    public void close() {
+        driver.close();
+    }
+}
