544484ffdc149e7844e2d08490e8b04c40139160
Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>
2023-09-07 17:29:41 +0300

    homework 26 refactoring

--- src/main/java/ua/hillel/homework/hw27/Homework27.java
+++ src/main/java/ua/hillel/homework/hw27/Homework27.java
@@ -1,53 +1,52 @@
 package ua.hillel.homework.hw27;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 import java.util.List;
 
-import static java.lang.Thread.sleep;
-
-public class Homework27 extends InitialDriver{
+public class Homework27 extends InitialDriver {
 
     @Test
-    public void testTopPurchases() throws InterruptedException {
+    public void testTopPurchases() {
 
         driver.get("https://rozetka.com.ua/");
+
         WebElement laptopAndComputers = driver
                 .findElement(By.xpath("//ul[@class='menu-categories menu-categories_type_main']/li[1]"));
         laptopAndComputers.click();
         WebElement laptops = driver.findElement(By.xpath("//a[@title='Ноутбуки'][1]"));
         laptops.click();
+
         WebElement filterRozetkaSeller = driver.findElement(By.xpath("//a[@data-id='Rozetka']"));
         filterRozetkaSeller.click();
-        sleep(3000);
 
-        WebElement filterPriceMax = driver.findElement(By.xpath("//input[@formcontrolname='max']"));
+        WebElement isRozetkaFilterApplied = driver.findElement(By.xpath("//*[@class='catalog-selection__link']"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(isRozetkaFilterApplied));
+
+        WebElement filterPriceMax = driver.findElement(By.xpath("//input[contains(@class, 'slider-filter__input')][2]"));
         filterPriceMax.clear();
         filterPriceMax.sendKeys("100000");
-        WebElement firstElementTopPurchases = driver.findElement(By.xpath("//button[@type='submit']"));
+
+        WebElement firstElementTopPurchases = webDriverWait.
+                until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@type='submit']")));
         firstElementTopPurchases.click();
+        WebElement isPriceFilterSelected = driver.findElement(By.xpath("//a[@class='catalog-selection__link']"));
+        webDriverWait.until(ExpectedConditions.visibilityOf(isPriceFilterSelected));
 
         List<WebElement> elements = driver
                 .findElements(By.xpath("//span[text()=' ТОП ПРОДАЖ ']/ancestor::div[@class='goods-tile__inner']"));
-
-        if(!elements.isEmpty()){
+        if (!elements.isEmpty()) {
             elements.get(0).click();
-            sleep(3000);
         } else {
             System.out.println("Elements not found");
         }
 
         WebElement containsTopPurchases = driver.findElement(By.xpath("(//span[contains(text(),'ТОП ПРОДАЖ')])[2]"));
-
+        webDriverWait.until(ExpectedConditions.visibilityOf(containsTopPurchases));
         Assert.assertTrue(containsTopPurchases.isDisplayed(), "Logo is not displayed");
 
     }
--- src/main/java/ua/hillel/homework/hw27/InitialDriver.java
+++ src/main/java/ua/hillel/homework/hw27/InitialDriver.java
@@ -3,6 +3,8 @@ package ua.hillel.homework.hw27;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
@@ -10,12 +12,16 @@ import java.time.Duration;
 
 public class InitialDriver {
     protected WebDriver driver;
+    protected WebDriverWait webDriverWait;
 
     @BeforeTest
     public void setupWebDriver() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(5000));
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(15000));
+        webDriverWait = new WebDriverWait(driver, Duration.ofMillis(5000));
+        driver.manage().window().maximize();
+
     }
     @AfterTest
     public void close() {
