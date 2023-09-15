a13a07ee7a6b86c00e628bff909c6e947e1a7f01
Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>
2023-09-15 12:55:34 +0300

    upd

--- src/main/java/ua/hillel/homework/hw27/InitialDriver.java
+++ src/main/java/ua/hillel/homework/hw27/InitialDriver.java
@@ -4,15 +4,9 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import java.time.Duration;
 
 public class InitialDriver {
@@ -22,10 +16,14 @@ public class InitialDriver {
     @BeforeTest
     public void setupWebDriver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(50000));
-        webDriverWait = new WebDriverWait(driver, Duration.ofMillis(50000));
-        //driver.manage().window().maximize();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-web-security");
+        options.addArguments("--incognito");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+        webDriverWait = new WebDriverWait(driver, Duration.ofMillis(10000));
+        driver.manage().window().maximize();
 
     }
     @AfterTest
