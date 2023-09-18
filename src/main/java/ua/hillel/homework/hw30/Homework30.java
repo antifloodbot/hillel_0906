538f15c97e77970d9dd44eabec2b0497c79ffc1c
Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>
2023-09-18 14:45:07 +0300

    fix homework 30 - changed navigation back to main page driver.navigate().back();
    - created constant for xpath and separate private method getPromotionalItems in order to remove code duplicate

--- src/main/java/ua/hillel/homework/hw30/Homework30.java
+++ src/main/java/ua/hillel/homework/hw30/Homework30.java
@@ -5,16 +5,18 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ua.hillel.homework.hw27.InitialDriver;
+
 import java.util.List;
 
 public class Homework30 extends InitialDriver {
 
     private final String BASE_URL = "https://rozetka.com.ua/";
+    private final String PROMOTIONAL_OFFERS_XPATH = "//h2[contains(text(),'Акционные предложения')]/following-sibling::ul/li";
 
     @Test
     public void testPromotionalOffers() {
         driver.get(BASE_URL);
-        List<WebElement> items = driver.findElements(By.xpath("//h2[contains(text(),'Акционные предложения')]/following-sibling::ul/li"));
+        List<WebElement> items = getPromotionalItems();
 
         Assert.assertEquals(items.size(), 6, "Amount of items is not 6");
 
@@ -26,12 +28,15 @@ public class Homework30 extends InitialDriver {
 
         Assert.assertEquals(clickedPrice, firstProductExpectedPrice, "Price is different");
 
-        driver.get(BASE_URL);
+        driver.navigate().back();
         String currentUrl = driver.getCurrentUrl();
         Assert.assertEquals(currentUrl, BASE_URL, "URLs are not the same");
-
-        List<WebElement> newItems = driver.findElements(By.xpath("//h2[contains(text(),'Акционные предложения')]/following-sibling::ul/li"));
+        List<WebElement> newItems = getPromotionalItems();
 
         Assert.assertEquals(newItems.size(), 6, "Amount of items is not 6");
+    }
+
+    private List<WebElement> getPromotionalItems() {
+        return driver.findElements(By.xpath(PROMOTIONAL_OFFERS_XPATH));
     }
 }
