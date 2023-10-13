c22d7de78ee4b28db52f8620995ae775431267b0
Kostiantyn Talamaniuk <ktalamaniuk@playtika.com>
2023-10-14 01:38:04 +0300

    lesson32

--- src/main/java/ua/hillel/homework/hw27/InitialDriver.java
+++ src/main/java/ua/hillel/homework/hw27/InitialDriver.java
@@ -3,8 +3,6 @@ package ua.hillel.homework.hw27;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
